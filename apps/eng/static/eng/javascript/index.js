d1409d467308b025203fb40b2d7a6437676ff991
ISAACLA <yanbinla@gmail.com>
2017-10-13 12:19:03 -0700

    more

--- apps/eng/static/eng/javascript/index.js
+++ apps/eng/static/eng/javascript/index.js
@@ -1,6 +1,33 @@
 $(document).ready(function(){
   $('.carousel').carousel({
-    interval: 1000
+    interval: 2000
   });
   $('.dropdown-toggle').dropdown();
+
+  var color = 'one';
+  var counter = 0;
+  $('.desc').hide();
+  $('.hexagon').hover(
+    function() {
+      $(this).find('.desc').fadeIn('fast');
+      counter++;
+      if (counter === 0) {
+        color = 'base';
+      } else if (counter === 1) {
+        color = 'one';
+      } else if (counter === 2) {
+        color = 'two';
+      } else if (counter === 3) {
+        color = 'three';
+      } else if (counter >= 4){
+        counter = 0 ;
+        color = 'base';
+      }
+      $(this).find('.desc').addClass(color);
+    },
+    function() {
+      $(this).find('.desc').fadeOut('fast', function() {
+        $(this).removeClass(color);
+      });
+    });
 })
