7db5141eff1289c74b790195582b66e7f8c8b526
ISAACLA <yanbinla@gmail.com>
2017-10-17 15:23:21 -0700

    revise index

--- apps/eng/views.py
+++ apps/eng/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render, redirect, HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, 'eng/index.html')
+    return render(request, 'eng/index/index.html')
 
 def aboutus(request):
-    return render(request, 'eng/aboutus.html')
+    return render(request, 'eng/aboutus/aboutus.html')
 
 def service(request):
     return render(request, 'eng/service/service.html')
