abbdf4257d783eb4bcc1e71d8786e9a71ef3f724
ISAACLA <yanbinla@gmail.com>
2017-10-17 17:25:54 -0700

    revise service page

--- apps/eng/static/eng/javascript/service/service.js
+++ apps/eng/static/eng/javascript/service/service.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
   $('.scrollspy').scrollSpy();
 });
+
+// var $window = window;
+var sidebar = document.querySelector('#sidebar');
+
+var offset = 50;
+
+window.addEventListener('scroll', function() {
+    // console.log('scroll');
+    if(window.scrollY >= offset) {
+        sidebar.style.marginTop = window.scrollY-offset+10 + 'px';
+    } else {
+        sidebar.style.marginTop = '0px';
+    }
+});
